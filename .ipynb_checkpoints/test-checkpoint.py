3a26462885b8748e99090afbbec80d2bdd1b87af
Lional <1248613656@qq.com>
2025-02-27 18:07:13 +0800

    Remove unwanted files from repository

--- .ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import torch
-from eval import evaluate_model_partial
-
-def test(model, test_loader, model_path):
-    model.load_state_dict(torch.load(model_path))
-    model.eval()
-    f1, iou = evaluate_model_partial(model, test_loader)
-    print(f"Test F1 Score: {f1:.4f}, Test IoU: {iou:.4f}")
-    return f1, iou
